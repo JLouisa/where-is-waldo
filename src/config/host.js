d458b65cc2fa85eb85950f567f961508fcee2083
Jlouisa <jonathan.louisa@gmail.com>
2023-11-20 03:34:36 +0100

    Refactor all the image to the same size & quality

--- src/config/host.js
+++ src/config/host.js
@@ -1,6 +1,6 @@
 function apiHost() {
   const host = "http://localhost:4000/waldo";
-  //   const host = "https://blog-api-production-175c.up.railway.app/waldo";
+  // const host = "https://blog-api-production-175c.up.railway.app/waldo";
   return host;
 }
 
